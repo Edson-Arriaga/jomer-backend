ebeceb950065259490f3d2ab9d6cbd5f10ffa4ba
Edson Arriaga <edson.arriaga.20@gmail.com>
2024-08-04 14:42:45 -0600

    Prepare for deploy

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyAhLrCVe4SOnHvoXC0CJ3LATe347z3b0UA",
-    authDomain: "jomer-ba42e.firebaseapp.com",
-    projectId: "jomer-ba42e",
-    storageBucket: "jomer-ba42e.appspot.com",
-    messagingSenderId: "887340336394",
-    appId: "1:887340336394:web:cccb71a25f6a86975e66f3",
-    measurementId: "G-33MXT1CLQJ"
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 };
 
 const app = initializeApp(firebaseConfig);
