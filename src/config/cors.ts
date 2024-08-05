0051c7e15858e3d49589776d28f1ad6a5e6ee726
Edson Arriaga <edson.arriaga.20@gmail.com>
2024-08-05 11:27:22 -0600

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from 'cors'
  
 export const corsConfig : CorsOptions = {
     origin: function(origin, callback){
-        const whiteList = [process.env.FRONTEND_URL_LOCAL, process.env.FRONTEND_URL_PRODUCTION]
+        const whiteList = [process.env.FRONTEND_URL_LOCAL]
         console.log("ORIGEN: " + origin)
         if(process.argv[2] === '--api'){
             whiteList.push(undefined)
