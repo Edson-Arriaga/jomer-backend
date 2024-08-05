327be288ab73002a3f78a878b32b9b3dd5f1f733
Edson Arriaga <edson.arriaga.20@gmail.com>
2024-08-05 10:14:35 -0600

    S

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,8 +2,8 @@ import { CorsOptions } from 'cors'
  
 export const corsConfig : CorsOptions = {
     origin: function(origin, callback){
-        const whiteList = [process.env.FRONTEND_URL]
-
+        const whiteList = [process.env.FRONTEND_URL_LOCAL, process.env.FRONTEND_URL_PRODUCTION]
+        console.log("ORIGEN: " + origin)
         if(process.argv[2] === '--api'){
             whiteList.push(undefined)
         }
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import fileUpload from 'express-fileupload';
 import dotenv from 'dotenv'
 import cors from 'cors'
+import morgan from 'morgan'
 import pieceRoutes from './routes/pieceRoutes'
 import adminRouter from './routes/adminRoutes';
 import { corsConfig } from './config/cors'
@@ -11,11 +12,16 @@ dotenv.config()
 connectDB()
 
 const app = express()
-app.use(cors(corsConfig))
+app.use(morgan('dev'))
 
+app.use(cors(corsConfig))
 app.use(fileUpload())
 
 app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({message: "CONECTADO"})
+})
 
 app.use('/api/pieces', pieceRoutes)
 app.use('/api/admin', adminRouter)
