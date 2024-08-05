f20c6ca7adeb1a240ee33bb706119d990387d37f
Edson Arriaga <edson.arriaga.20@gmail.com>
2024-08-05 12:38:19 -0600

    Try get IP

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import pieceRoutes from './routes/pieceRoutes'
 import adminRouter from './routes/adminRoutes';
 import { corsConfig } from './config/cors'
 import { connectDB } from './config/db';
+import axios from 'axios';
 
 dotenv.config()
 connectDB()
@@ -19,9 +20,14 @@ app.use(fileUpload())
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.json({message: "CONECTADO"})
-})
+app.get('/', async (req, res) => {
+    try {
+      const response = await axios.get('https://api.ipify.org?format=json');
+      res.send(`Your IP is: ${response.data.ip}`);
+    } catch (error) {
+      res.status(500).send('Error getting IP address');
+    }
+  });
 
 app.use('/api/pieces', pieceRoutes)
 app.use('/api/admin', adminRouter)
