7f8b63b67e3db9295c1283288ada0c97c7151888
Edson Arriaga <edson.arriaga.20@gmail.com>
2024-08-05 20:14:22 -0600

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from 'cors'
 
 export const corsConfig : CorsOptions = {
     origin: function(origin, callback){
-        const whiteList = ["https://jomer-frontend-p4v9bczqk-edson-arriagas-projects.vercel.app"]
+        const whiteList = [process.env.FRONTEND_URL]
 
         if(process.argv[2] === '--api'){
             whiteList.push(undefined)
