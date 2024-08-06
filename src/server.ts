30259d48af9a79419c4f03cf1b452f6d3c32929b
Edson Arriaga <edson.arriaga.20@gmail.com>
2024-08-05 19:46:06 -0600

    Origin

--- src/server.ts
+++ src/server.ts
@@ -20,15 +20,6 @@ app.use(fileUpload())
 
 app.use(express.json())
 
-app.get('/', async (req, res) => {
-    try {
-      const response = await axios.get('https://api.ipify.org?format=json');
-      res.send(`Your IP is: ${response.data.ip}`);
-    } catch (error) {
-      res.status(500).send('Error getting IP address');
-    }
-  });
-
 app.use('/api/pieces', pieceRoutes)
 app.use('/api/admin', adminRouter)
 
