458a2b32b1deaf0bd284eafdfc894e03f074836a
Edson Arriaga <edson.arriaga.20@gmail.com>
2024-08-05 19:36:21 -0600

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,12 +2,13 @@ import { CorsOptions } from 'cors'
  
 export const corsConfig : CorsOptions = {
     origin: function(origin, callback){
-        const whiteList = [process.env.FRONTEND_URL, process.env.BACKEND_URL]
+        const whiteList = [process.env.FRONTEND_URL]
+        console.log("ORIGEN: " + origin)
         if(process.argv[2] === '--api'){
             whiteList.push(undefined)
         }
 
-        if(whiteList.includes(origin)){
+        if(whiteList.includes(origin || process.env.BACKEND_URL)){
             callback(null, true)
         } else {
             callback(new Error('Error de CORS'))
