7f3a1058e276e5f87cc136066763c7bfad2ccab8
Edson Arriaga <edson.arriaga.20@gmail.com>
2024-08-24 07:23:15 -0600

    Cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,11 +3,10 @@ import { CorsOptions } from 'cors'
 export const corsConfig : CorsOptions = {
     origin: function(origin, callback){
         const whiteList = [process.env.FRONTEND_URL]
-
+        console.log(origin)
         if(process.argv[2] === '--api'){
             whiteList.push(undefined)
         }
-
         if(whiteList.includes(origin) || !origin){
             callback(null, true)
         } else {
